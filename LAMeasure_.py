c00f4ba8b1e7b2cecb1f2ab1783ab7cf929f7daf
whatalnk <watnk0806@gmail.com>
2016-07-10 23:03:49 +0900

    Measure leaf area

---
+++ LAMeasure_.py
@@ -0,0 +1,53 @@
+from __future__ import with_statement
+
+import os.path
+import fnmatch
+import datetime
+
+import java.lang.Float as JFloat
+
+from ij import IJ, ImagePlus
+from ij.process import ImageProcessor, AutoThresholder
+from ij.plugin.filter import ThresholdToSelection
+from ij.plugin.filter import ParticleAnalyzer as PA
+from ij.measure import ResultsTable
+
+# PA args and options
+MINSIZE = 1000
+MAXSIZE = JFloat.POSITIVE_INFINITY
+options = PA.SHOW_NONE
+
+dir = IJ.getDirectory("Path to directory")
+
+filenames = [os.path.join(dir, file) for file in os.listdir(dir) if fnmatch.fnmatch(file, '*.jpg')]
+filecounter = 0
+
+for filename in filenames:
+    ip = IJ.openImage(filename).getProcessor().convertToByteProcessor()
+    IJ.log("Input file: %s" % filename)
+
+    ip.setAutoThreshold("Minimum")
+
+    roi = ThresholdToSelection().convert(ip)
+    ip.setRoi(roi)
+    mask = ip.getMask()
+
+    rt = ResultsTable()
+
+    pa = PA(options, PA.AREA + PA.PERIMETER + PA.CIRCULARITY, rt, MINSIZE, MAXSIZE) 
+    pa.analyze(ImagePlus(), mask)
+
+    filebasename = os.path.basename(filename)
+
+    nrow = rt.size()
+    for i in range(nrow):
+        rt.setValue("Filename", i, filebasename)
+
+    outfilename = os.path.join(dir, "res_%s.csv" % os.path.splitext(filebasename)[0])
+    rt.save(outfilename)
+    IJ.log("Result: %s" % outfilename)
+
+    maskfilename = os.path.join(dir, "mask_%s" % filebasename)
+    IJ.save(ImagePlus(filebasename, mask), maskfilename)
+    IJ.log("Mask image: %s" % maskfilename)
+    
