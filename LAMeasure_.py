bb50a06153deec11bb4377b0912c026082b82722
whatalnk <watnk0806@gmail.com>
2016-07-11 18:04:04 +0900

    for module import

--- LAMeasure_.py
+++ LAMeasure_.py
@@ -26,73 +26,74 @@ class PAResult():
     def asRow(self):
         return "%s, %f, %f, %f, %f, %f, %f\n" % (self.filename, self.area, self.perim, self.circ, self.ar, self.round, self.solidity)
 
-# PA args and options
-MINSIZE = 1000
-MAXSIZE = JFloat.POSITIVE_INFINITY
-options = PA.SHOW_NONE
+if __name__ == '__main__':
+    # PA args and options
+    MINSIZE = 1000
+    MAXSIZE = JFloat.POSITIVE_INFINITY
+    options = PA.SHOW_NONE
 
-DistPixel = IJ.getNumber("Distance in pixel", 600)
-DistCm = IJ.getNumber("Distance in cm", 2.54)
-cal = Calibration()
-cal.setUnit("cm")
-cal.pixelWidth = DistCm / DistPixel
-cal.pixelHeight = DistCm / DistPixel
-ImagePlus().setGlobalCalibration(cal)
+    DistPixel = IJ.getNumber("Distance in pixel", 600)
+    DistCm = IJ.getNumber("Distance in cm", 2.54)
+    cal = Calibration()
+    cal.setUnit("cm")
+    cal.pixelWidth = DistCm / DistPixel
+    cal.pixelHeight = DistCm / DistPixel
+    ImagePlus().setGlobalCalibration(cal)
 
-dir = IJ.getDirectory("Path to directory")
+    dir = IJ.getDirectory("Path to directory")
 
-filenames = [os.path.join(dir, file) for file in os.listdir(dir) if fnmatch.fnmatch(file, '*.jpg')]
+    filenames = [os.path.join(dir, file) for file in os.listdir(dir) if fnmatch.fnmatch(file, '*.jpg')]
 
-maskdir = os.path.join(dir, "mask")
-resdir = os.path.join(dir, "res")
+    maskdir = os.path.join(dir, "mask")
+    resdir = os.path.join(dir, "res")
 
-for p in [maskdir, resdir]:
-    if not os.path.exists(p):
-        os.makedirs(p)
+    for p in [maskdir, resdir]:
+        if not os.path.exists(p):
+            os.makedirs(p)
 
-leafnumbers = []
+    leafnumbers = []
 
-for filename in filenames:
-    ip = IJ.openImage(filename).getProcessor().convertToByteProcessor()
-    IJ.log("Input file: %s" % filename)
+    for filename in filenames:
+        ip = IJ.openImage(filename).getProcessor().convertToByteProcessor()
+        IJ.log("Input file: %s" % filename)
 
-    ip.setAutoThreshold("Minimum")
+        ip.setAutoThreshold("Minimum")
 
-    roi = ThresholdToSelection().convert(ip)
-    ip.setRoi(roi)
-    mask = ip.getMask()
+        roi = ThresholdToSelection().convert(ip)
+        ip.setRoi(roi)
+        mask = ip.getMask()
 
-    rt = ResultsTable()
-    rt.showRowNumbers(False)
-    pa = PA(options, PA.AREA + PA.PERIMETER + PA.CIRCULARITY, rt, MINSIZE, MAXSIZE) 
-    pa.analyze(ImagePlus(), mask)
+        rt = ResultsTable()
+        rt.showRowNumbers(False)
+        pa = PA(options, PA.AREA + PA.PERIMETER + PA.CIRCULARITY, rt, MINSIZE, MAXSIZE) 
+        pa.analyze(ImagePlus(), mask)
 
-    filebasename = os.path.basename(filename)
+        filebasename = os.path.basename(filename)
 
-    paResults = [] 
-    nrow = rt.size()
-    for i in range(nrow):
-        area = rt.getValue("Area", i)
-        perim = rt.getValue("Perim.", i)
-        circ = rt.getValue("Circ.", i)
-        ar = rt.getValue("AR", i)
-        round = rt.getValue("Round", i)
-        solidity = rt.getValue("Solidity", i)
-        paResults.append(PAResult(filebasename, area, perim, circ, ar, round, solidity))
+        paResults = [] 
+        nrow = rt.size()
+        for i in range(nrow):
+            area = rt.getValue("Area", i)
+            perim = rt.getValue("Perim.", i)
+            circ = rt.getValue("Circ.", i)
+            ar = rt.getValue("AR", i)
+            round = rt.getValue("Round", i)
+            solidity = rt.getValue("Solidity", i)
+            paResults.append(PAResult(filebasename, area, perim, circ, ar, round, solidity))
 
-    outfilename = os.path.join(resdir, "res_%s.csv" % os.path.splitext(filebasename)[0])
-    with codecs.open(outfilename, "w", "utf-8") as f:
-        header = ["Filename", "Area", "Perim.", "Circ", "AR", "Round", "Solidity"]
-        table = [",".join(header) + "\n"]
-        table += [row.asRow() for row in paResults]
-        f.writelines(table)
-    IJ.log("Result: %s" % outfilename)
+        outfilename = os.path.join(resdir, "res_%s.csv" % os.path.splitext(filebasename)[0])
+        with codecs.open(outfilename, "w", "utf-8") as f:
+            header = ["Filename", "Area", "Perim.", "Circ", "AR", "Round", "Solidity"]
+            table = [",".join(header) + "\n"]
+            table += [row.asRow() for row in paResults]
+            f.writelines(table)
+        IJ.log("Result: %s" % outfilename)
 
-    maskfilename = os.path.join(maskdir, "mask_%s" % filebasename)
-    IJ.save(ImagePlus(filebasename, mask), maskfilename)
-    IJ.log("Mask image: %s" % maskfilename)
-    
-    leafnumbers.append((filebasename, nrow))
+        maskfilename = os.path.join(maskdir, "mask_%s" % filebasename)
+        IJ.save(ImagePlus(filebasename, mask), maskfilename)
+        IJ.log("Mask image: %s" % maskfilename)
+        
+        leafnumbers.append((filebasename, nrow))
 
-with codecs.open(os.path.join(dir, "leafnumbers.csv"), "w", "utf-8") as f:
-    f.writelines(["%s, %d\n" % fn for fn in leafnumbers])
+    with codecs.open(os.path.join(dir, "leafnumbers.csv"), "w", "utf-8") as f:
+        f.writelines(["%s, %d\n" % fn for fn in leafnumbers])
